6e247e4d11fc5c758d4bfab4dc568c73d9542c3a
RogelioSA <rsihuayro@gmail.com>
2026-03-27 09:14:31 -0500

    aPI

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  //apiUrl: 'https://apis.perumotor.com.pe/api'
-  apiUrl: 'https://localhost:8085/api'
+  apiUrl: 'https://apis.perumotor.com.pe/api'
+  //apiUrl: 'https://localhost:8085/api'
 };
