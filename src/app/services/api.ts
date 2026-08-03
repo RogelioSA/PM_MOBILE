99a6af5ce8fe7b8dc9ec1ce9c01b6b3fd0b8b539
RogelioSA <106559124+RogelioSA@users.noreply.github.com>
2026-08-03 09:14:21 -0500

    chore: mostrar payload de justificación

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -73,6 +73,22 @@ export interface MotivoJustificacion {
   descripcion: string;
 }
 
+export interface OtrosDocumentosPayload {
+  idEmpresa: string;
+  idOtrosDocumentos: string;
+  tipo: string;
+  codigoPersonal: string;
+  idMotivo: string;
+  fechaDesde: string;
+  fechaHasta: string;
+  descripcion: string;
+  nroCertificado: string;
+  centroMedico: string;
+  idSucursal: string;
+  idEmisor: string;
+  idPlanilla: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
@@ -798,6 +814,22 @@ export class Api {
     return this.https.get<any>(
       `${this.baseUrl}/Personal/motivos-justificacion`,
       { headers: this.authService.getHeaders() }
+    );
+  }
+
+  guardarOtrosDocumentos(payload: OtrosDocumentosPayload): Observable<any> {
+    console.log(
+      '[POST /Personal/otros-documentos] Body:',
+      JSON.stringify(payload, null, 2)
+    );
+
+    return this.https.post<any>(
+      `${this.baseUrl}/Personal/otros-documentos`,
+      payload,
+      { headers: this.authService.getHeaders() }
+    ).pipe(
+      map(response => response),
+      catchError(error => throwError(() => error))
     );
   }
 
